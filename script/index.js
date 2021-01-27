f915fe3de9bdf018b2341eafc325a402581ab275
Akniet Adil <akonstudy@gmail.com>
2021-01-27 12:03:02 +0600

    main page front

---
+++ script/index.js
@@ -0,0 +1,5 @@
+// functioon to change text in dropdown btn "Whole Kazakhstan"(in choose location) after choosing city from dropdown items
+function cityBtn(cityName) {
+    $("#choosedCityName").empty();
+    $("#choosedCityName").append(cityName);
+}
